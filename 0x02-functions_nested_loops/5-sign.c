a03834df0228a9f9336019463fe2cf5185b4dbe7
imadovetch <ibouderoua63@gmail.com>
2023-06-20 11:14:02 -0700

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,19 +8,19 @@
  * -1 and prints - if n is less than zero
  */
 int print_sign(int n)
+
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43);
 		return (1);
-	} else if (n == 0)
-}
+	} else if (n == 0){
+
 		_putchar(48);
 		return (0);
 	} else if (n < 0)
 	{
-		_putchar('-');
+		_putchar(45);
 	}
-		return (-1);
+	return (-1);
 }
-
