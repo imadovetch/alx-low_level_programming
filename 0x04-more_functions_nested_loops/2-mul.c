523401d23958bc3b9419c51fe3eb443f16df08d7
imadovetch <ibouderoua63@gmail.com>
2023-06-22 10:35:52 -0700

    2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,11 +2,12 @@
 
 /**
  * mul - multiply a by b
- * @c: is the char to be checked
- * Return: a *b 
+ * @a: is the first int
+ * @b: is the second int
+ * Return: a * b
  */
 
 int mul(int a, int b)
 {
-	return a * b;
+	return (a * b);
 }
