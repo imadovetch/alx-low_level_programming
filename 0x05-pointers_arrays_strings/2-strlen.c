d648a47bc5974a713e9be92cfba9e5cacfab543e
imadovetch <ibouderoua63@gmail.com>
2023-06-26 10:14:15 -0700

    3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - count the size 
+ * @s: string
+ * Return: lenght
+ */
+int _strlen(char *s)
+{
+	int lenght;
+	for( s=0 ;s != \0; s++ )
+	{
+		lenght++;
+	}
+	return (lenght);
+}
+
