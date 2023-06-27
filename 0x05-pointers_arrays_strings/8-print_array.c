e9d0df92c7d1fc0413685e78ed95cc19925b93f5
imadovetch <ibouderoua63@gmail.com>
2023-06-26 17:52:05 -0700

    8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,10 +9,12 @@
 
 void print_array(int *a, int n)
 {
+	int i;
+
 	   if (a == NULL)
         return;
 
-    for (int i = 0; i < n; i++)
+    for (i = 0; i < n; i++)
     {
         printf("%d", a[i]);
         if (i != n - 1)
