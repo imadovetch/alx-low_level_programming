8c387340d853f6fdec253ddaa68466af8c2669e5
imadovetch <ibouderoua63@gmail.com>
2023-06-28 20:30:57 -0700

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
@@ -11,22 +10,22 @@
  */
 char *_strcat(char *dest, char *src)
 {
-README.md int i = 0, j = 0;
+	int i = 0, j = 0;
 
-README.md /* Finding the end of the destination string */
-README.md while (dest[i] != '\0')
-README.md README.md i++;
+	/* Finding the end of the destination string */
+	while (dest[i] != '\0')
+		i++;
 
-README.md /* Appending the source string to the destination string */
-README.md while (src[j] != '\0')
-README.md {
-README.md README.md dest[i] = src[j];
-README.md README.md i++;
-README.md README.md j++;
-README.md }
+	/* Appending the source string to the destination string */
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
-README.md /* Null-terminating the concatenated string */
-README.md dest[i] = '\0';
+	/* Null-terminating the concatenated string */
+	dest[i] = '\0';
 
-README.md return (dest);
+	return (dest);
 }
