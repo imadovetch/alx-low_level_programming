4976d649ca33740179098f6f23fbad233cc015d7
imadovetch <ibouderoua63@gmail.com>
2023-07-10 06:49:47 -0700

    3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,6 +12,9 @@ int **alloc_grid(int width, int height)
 	int **array;
 	int i, j;
 
+	if (height <= 0 || width <= 0)
+		return (NULL);
+
 	array = (int **)malloc(height * sizeof(int *));
 	if (array == NULL)
 	{
