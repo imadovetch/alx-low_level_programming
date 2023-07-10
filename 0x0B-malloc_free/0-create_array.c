2a528d55903de512ffd583307aa25d9d521fc494
imadovetch <ibouderoua63@gmail.com>
2023-07-09 23:32:04 -0700

    the last update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,14 +10,14 @@
 char *create_array(unsigned int size, char c)
 {
 	char *ptr;
-	unsigned i;
+	unsigned int i;
 
 	ptr = malloc(sizeof(char) * size);
 
 	if (size == 0 || ptr == NULL)
 		return (NULL);
 
-	for (i = 0;i < size; i++)
+	for (i = 0; i < size; i++)
 		ptr[i] = c;
 
 	return (ptr);
