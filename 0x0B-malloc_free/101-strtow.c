07804021e464e1107634fafbf88ee34c26c1355f
imadovetch <ibouderoua63@gmail.com>
2023-07-11 09:15:25 -0700

    101 make it smaller

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * strtow - Splits a string into an array of words.
  * @str: The input string.
@@ -9,42 +10,43 @@
  */
 char **strtow(char *str)
 {
-	int i = 0, num_words = 0, start = -1, end;
-	char **array;
+    int i = 0, num_words = 0, start = -1, end;
+    char **array;
 
-	if (str == NULL || str[0] == '\0')
-		return (NULL);
-	while (str[i])
-	{
-		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
-			num_words++;
-		i++; }
-	array = malloc(sizeof(char *) * (num_words + 1));
-	if (array == NULL)
-		return (NULL);
-	for (i = num_words = 0; str[i]; i++)
-	{
-		if (str[i] != ' ' && start < 0)
-			start = i;
-		if (str[i] == ' ' && start >= 0)
-		{
-			end = i;
-			array[num_words] = malloc(sizeof(char) * (end - start + 1));
-			if (array[num_words] == NULL)
-				return (NULL);
-			array[num_words] = strncpy(array[num_words], &str[start], end - start);
-			array[num_words][end - start] = '\0';
-			num_words++;
-			start = -1; } }
-	if (start >= 0)
-	{
-		array[num_words] = malloc(sizeof(char) * (i - start + 1));
-		if (array[num_words] == NULL)
-			return (NULL);
-		array[num_words] = strncpy(array[num_words], &str[start], i - start);
-		array[num_words][i - start] = '\0';
-		num_words++; }
+    if (str == NULL || str[0] == '\0')
+        return (NULL);
+    while (str[i])
+    {
+        if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+        {
+            if (str[i + 1] == ' ' || str[i + 1] == '\0')
+                num_words++;
+        }
+        i++; 
+    }
 
-	array[num_words] = NULL;
-	return (array); }
+    array = malloc(sizeof(char *) * (num_words + 1));
+    if (array == NULL)
+        return (NULL);
+
+    for (i = num_words = 0; str[i]; i++)
+    {
+        if (str[i] != ' ' && start < 0)
+            start = i;
+        if ((str[i] == ' ' || str[i + 1] == '\0') && start >= 0)
+        {
+            end = i;
+            array[num_words] = malloc(sizeof(char) * (end - start + 2));
+            if (array[num_words] == NULL)
+                return (NULL);
+            array[num_words] = strncpy(array[num_words], &str[start], end - start + 1);
+            array[num_words][end - start + 1] = '\0';
+            num_words++;
+            start = -1; 
+        } 
+    }
+
+    array[num_words] = NULL;
+    return (array); 
+}
 
