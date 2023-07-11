fa4cc98f928c72abd4d29e9434f8c221683ae7c5
imadovetch <ibouderoua63@gmail.com>
2023-07-11 04:21:54 -0700

    100

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * argstostr - Concatenates command-line arguments into a single string
+ * @argc: The number of command-line arguments
+ * @argv: An array of strings representing the arguments
+ *
+ * Return: A dynamically allocated string containing the concatenated,
+ *         with each argument separated by a newline character '\n',
+ *         or NULL if memory allocation fails.
+ */
+char *argstostr(int argc, char **argv)
+{
+
+	int c, h, i, g;
+
+	for (h = 1; h < argc; h++)
+	{
+		for (g = 0; argv[h][g] != '\0'; g++)
+		{
+			c++;
+		}
+		c += 2;
+	}
+
+	char *str = malloc(sizeof(char) * (c + 1));
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	int index = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (g = 0; argv[i][g] != '\0'; g++)
+		{
+			str[index] = argv[i][g];
+			index++;
+		}
+		str[index] = '\n';
+		index++;
+	}
+
+	str[index] = '\0';
+	return (str);
+}
+
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,3 +3,4 @@
 char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
 int **alloc_grid(int width, int height);
+char *argstostr(int ac, char **av);
