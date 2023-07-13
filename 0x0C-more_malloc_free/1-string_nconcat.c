f8eff0eaa7e9f4e8d28546cac10647416b85eb0f
imadovetch <ibouderoua63@gmail.com>
2023-07-13 09:15:37 -0700

    task 1 last update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,5 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 
 /**
  * string_nconcat - concatenates two strings
@@ -15,30 +13,37 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ptr;
-	unsigned int len1, x = 0;
+	unsigned int len1, len2, concat_len, i, j;
+	char *concat;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	len1 = strlen(s1);
-	ptr = malloc(len1 + n + 1);
-	if (ptr == NULL)
-		return NULL;
-	if (n >= strlen(s2))
-		n = strlen(s2);
-	for (; s1[x] != '\0'; x++)
-	{
-		ptr[x] = s1[x];
-	}
-	for (unsigned int i = 0; i < n; i++)
-	{
-		ptr[x] = s2[i];
-		x++;
-	}
-	ptr[x] = '\0';  // Ensure the new string is null-terminated
-	return ptr;
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+		;
+
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+		;
+
+	if (n >= len2)
+		n = len2;
+
+	concat_len = len1 + n;
+	concat = malloc(sizeof(char) * (concat_len + 1));
+
+	if (concat == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		concat[i] = s1[i];
+
+	for (j = 0; j < n; j++)
+		concat[i + j] = s2[j];
+
+	concat[i + j] = '\0';
+
+	return (concat);
 }
 
