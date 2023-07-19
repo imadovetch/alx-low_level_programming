6b70265d2baad5436a2e5cdbba5a594cc9003efd
imadovetch <ibouderoua63@gmail.com>
2023-07-19 22:32:13 +0100

    the task 4 update 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,41 +1,46 @@
 #include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
 
+/**
+ * main - Entry point of the program
+ * @argc: Argument count
+ * @argv: Array of command-line arguments
+ *
+ * Return: Exit status of the program
+ */
 int main(int argc, char *argv[])
 {
-	int num1;
-	int num2;
-	char *operation;
-	int (*func)(int, int);
+    int num1;
+    int num2;
+    char *operation;
+    int (*func)(int, int);
 
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+    if (argc != 4)
+    {
+        printf("Error\n");
+        exit(98);
+    }
 
-	num1 = atoi(argv[1]);
-	operation = argv[2];
-	num2 = atoi(argv[3]);
+    num1 = atoi(argv[1]);
+    operation = argv[2];
+    num2 = atoi(argv[3]);
 
-	if (operation == '%' || operation == '/')
-	{
-		if (num2 == 0)
-		{
-			printf("Error\n");
-			exit(100);
-		}
-	}
+    if (*operation == '%' || *operation == '/')
+    {
+        if (num2 == 0)
+        {
+            printf("Error\n");
+            exit(100);
+        }
+    }
 
-	func = get_op_func(operation);
-	if (func == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
+    func = get_op_func(operation);
+    if (func == NULL)
+    {
+        printf("Error\n");
+        exit(99);
+    }
 
-	printf("%d\n", func(num1, num2));
+    printf("%d\n", func(num1, num2));
 
-	return (0);
+    return 0;
 }
