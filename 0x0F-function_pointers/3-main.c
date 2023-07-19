15760b952f01f759e5ce43516801994ca4c4c551
imadovetch <ibouderoua63@gmail.com>
2023-07-19 22:38:21 +0100

    the task 4 update 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,27 +9,39 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1;
-	int num2;
-	char *operation;
+    int num1;
+    int num2;
+    char *operation;
+    int (*func)(int, int);
 
-	if (argc != 4)
-	{
-		printf("Error\n");
-		return (1);
-	}
+    if (argc != 4)
+    {
+        printf("Error\n");
+        exit(98);
+    }
 
-	if (*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*' && *argv[2] != '/' && *argv[2] != '%')
-	{
-		printf("Error\n");
-		return (1);
-	}
+    num1 = atoi(argv[1]);
+    operation = argv[2];
+    num2 = atoi(argv[3]);
 
-	num1 = atoi(argv[1]);
-	operation = argv[2];
-	num2 = atoi(argv[3]);
-
-	printf("%d\n", get_op_func(operation)(num1, num2));
-
-	return (0);
+    if (strcmp(operation == "%") == 0 || strcmp(operation == "/") == 0)
+    {
+        if (num2 == 0)
+{
+        printf("Error\n");
+        exit(100);
 }
+
+    }
+  
+    func = get_op_func(operation);
+    if (func == NULL)
+{
+  printf("Error\n");
+  exit(99);
+}
+    printf("%d\n", func(num1, num2));
+
+    return (0);
+}
+
