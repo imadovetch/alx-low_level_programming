e69497119e62d1ef599b21939ab2760afd4f4f96
imadovetch <ibouderoua63@gmail.com>
2023-07-19 18:16:31 +0100

    the first task

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * print_name - Calls a function to print a name
+ * @name: The name to be printed
+ * @f: Pointer to a function that takes a char pointer argument and returns void
+ *
+ * Description: This function calls the provided function to print the given name.
+ * If the name or the function pointer is NULL, nothing is done.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
