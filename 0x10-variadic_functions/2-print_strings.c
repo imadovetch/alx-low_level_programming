0eafb7a34aff7fc0d4e9b7c67d6da30953ee1c2a
imadovetch <ibouderoua63@gmail.com>
2023-07-20 22:29:17 +0100

    task 3 betyy

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,21 +9,23 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-    va_list ap;
-    unsigned int i;
+	va_list ap;
+	unsigned int i;
 
-    va_start(ap, n);
-    for (i = 0; i < n; i++)
-    {
-        const char *str = va_arg(ap, const char *);
-        if (str)
-            printf("%s", str);
-        else
-            printf("nil");
-        if (i < n - 1 && separator)
-            printf("%s", separator);
-    }
-    printf("\n");
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		const char *str = va_arg(ap, const char *);
+		if (str)
+			printf("%s", str);
+		else
+			printf("nil");
 
-    va_end(ap);
+		if (i < n - 1 && separator)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+
+	va_end(ap);
 }
