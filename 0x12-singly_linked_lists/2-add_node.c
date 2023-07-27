bee60541cf19c091c41d34518515b7e513757f7b
imadovetch <ibouderoua63@gmail.com>
2023-07-27 21:49:42 +0100

    task mandatory

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,78 +1,25 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-typedef struct list_s
-{
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-} list_t;
+#include "lists.h"
+/**
+ * add_node - Adds a new node at the end of a linked list.
+ * @head: Pointer to a pointer to the head of the list.
+ * @str: The string to be added to the new node.
+ *
+ * Return: Address of the new node, or NULL on failure.
+ */
+list_t *add_node(list_t **head, const char *str) {
+    list_t *new_node = malloc(sizeof(list_t));
+    if (!new_node)
+        return NULL;
 
-list_t *add_node(list_t **head, const char *str){
+    new_node->str = strdup(str);
+    if (new_node->str == NULL) {
+        free(new_node);
+        return NULL;
+    }
 
-	list_t *bNode =  malloc(sizeof(list_t));
-	if (!bNode)
-    	return (NULL);
-	bNode->str = str;
-  	bNode->next = NULL;
-	
-	return bNode;
-  
-}
-size_t print_list(const list_t *h)
-{
-	const list_t *current = h;
-	size_t count = 0;
+    new_node->len = strlen(str);
+    new_node->next = *head;
+    *head = new_node;
 
-	while (current != NULL)
-	{
-		if (current->str == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
-		else
-		{
-			printf("[%u] %s\n", current->len, current->str);
-		}
-		count++;
-		current = current->next;
-	}
-
-	return (count);
-}
-int main(void)
-{
-    list_t *head;
-
-    head = NULL;
-    add_node(&head, "Alexandro");
-    add_node(&head, "Asaia");
-    add_node(&head, "Augustin");
-    add_node(&head, "Bennett");
-    add_node(&head, "Bilal");
-    add_node(&head, "Chandler");
-    add_node(&head, "Damian");
-    add_node(&head, "Daniel");
-    add_node(&head, "Dora");
-    add_node(&head, "Electra");
-    add_node(&head, "Gloria");
-    add_node(&head, "Joe");
-    add_node(&head, "John");
-    add_node(&head, "John");
-    add_node(&head, "Josquin");
-    add_node(&head, "Kris");
-    add_node(&head, "Marine");
-    add_node(&head, "Mason");
-    add_node(&head, "Praylin");
-    add_node(&head, "Rick");
-    add_node(&head, "Rick");
-    add_node(&head, "Rona");
-    add_node(&head, "Siphan");
-    add_node(&head, "Sravanthi");
-    add_node(&head, "Steven");
-    add_node(&head, "Tasneem");
-    add_node(&head, "William");
-    add_node(&head, "Zee");
-    print_list(head);
-    return (0);
+    return new_node;
 }
---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - Frees the memory occupied by a linked list.
+ * @head: Pointer to the head of the list.
+ */
+void free_list(list_t *head)
+{
+	list_t *current = head;
+	list_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
+	}
+}
