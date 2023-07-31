64d09814fce15dafca944f1a8de83a6708afd1e6
imadovetch <ibouderoua63@gmail.com>
2023-08-01 00:57:21 +0100

    task reverse

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -11,6 +11,10 @@ listint_t *reverse_listint(listint_t **head)
 	listint_t *current = *head;
 	listint_t *next = NULL;
 	listint_t *prev = NULL;
+	if (!*head)
+		return (NULL);
+	if(current->next == NULL)
+		return *head;
 
 	while (current)
 	{
@@ -22,4 +26,23 @@ listint_t *reverse_listint(listint_t **head)
 	*head = prev;
 
 	return (prev);
+}
+int main(void)
+{
+    listint_t *head;
+
+    head = NULL;
+    add_nodeint_end(&head, 0);
+    add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 2);
+    add_nodeint_end(&head, 3);
+    add_nodeint_end(&head, 4);
+    add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 402);
+    add_nodeint_end(&head, 1024);
+    print_listint(head);
+    reverse_listint(&head);
+    print_listint(head);    
+    free_listint2(&head);
+    return (0);
 }
