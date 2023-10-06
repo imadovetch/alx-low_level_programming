1fdca2ba327b67089da97c527bf67d6e36a95b81
imadovetch <ibouderoua63@gmail.com>
2023-10-06 11:49:27 +0100

    push44

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -34,4 +34,5 @@ hash_node_t **array;
 
 unsigned long int hash_djb2(const unsigned char *str);
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
 #endif
